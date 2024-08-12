c17f5f756868ae7c1c7a155a06862ed41ef2da8d
sivaraman123456 <sivagithub123@gmail.com>
2024-08-12 14:44:02 +0530

    feature added

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Delete,
-  Patch,
-  Param,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post,Delete ,Patch,Param} from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateEmployeeDto } from '../dto/create-employee.dto';
@@ -18,6 +10,8 @@ export class EmployeeController {
 
   @Post()
   async createEmployee(@Body() data: CreateEmployeeDto) {
+    console.log(data);
+    
     return this.employeeService.createEmployee(data);
   }
 
@@ -26,20 +20,22 @@ export class EmployeeController {
     return this.employeeService.getEmployees();
   }
   @Get(':id')
-  async getDataById(@Param('id') id: number) {
-    return this.employeeService.getEmpData(Number(id));
+  async getDataById(@Param('id')id:number)
+  {
+    return this.employeeService.getEmpData(Number(id))
   }
 
   @Patch(':id')
-  async updateEmployees(
-    @Param('id') id: number,
-    @Body() UpdateEmployeeDto: UpdateEmployeeDto,
-  ) {
-    return this.employeeService.updateEmployee(Number(id), UpdateEmployeeDto);
+  async updateEmployees(@Param('id')id:number,
+  @Body() UpdateEmployeeDto: UpdateEmployeeDto ,) {
+    return this.employeeService.updateEmployee(Number(id),UpdateEmployeeDto);
   }
 
   @Delete(':id')
-  async delEmployee(@Param('id') id: number) {
-    return this.employeeService.deleteEmployee(Number(id));
-  }
+async delEmployee(@Param('id')id:number)
+{
+return this.employeeService.deleteEmployee(Number(id))
 }
+}
+
+
