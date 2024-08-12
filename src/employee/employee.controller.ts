85f05b5d4324912ad8d3d06c93345c769d760392
sivaraman123456 <sivagithub123@gmail.com>
2024-08-12 14:10:49 +0530

    second commit

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -10,6 +10,8 @@ export class EmployeeController {
 
   @Post()
   async createEmployee(@Body() data: CreateEmployeeDto) {
+    console.log(data);
+    
     return this.employeeService.createEmployee(data);
   }
 
